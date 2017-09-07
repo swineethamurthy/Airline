1bed060a6c23a341f07a1e77074199a55735a550
Vaishali Purwar <talk.vaishali@gmail.com>
2017-09-07 12:05:34 +0530

    [swineetha/vaishali] User Story 3 : Included Travel Class in Search Criteria

--- src/main/java/airline/Services/FlightService.java
+++ src/main/java/airline/Services/FlightService.java
@@ -1,10 +1,13 @@
 package airline.Services;
 
 import airline.Models.FlightInformation;
+import airline.Models.FlightSearchCriteria;
 import airline.Models.Place;
 import airline.Models.TravelClass;
 import airline.Repositories.FlightInformationRepository;
 import airline.Repositories.PlaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -17,21 +20,22 @@ import java.util.stream.Collectors;
 @Service
 public class FlightService {
 
-    private FlightInformationRepository flightInformationRepository;
-    private PlaceRepository placeRepository;
+    @Autowired @Qualifier("FlightsRepository")
+    private FlightInformationRepository flightsInformationRepository;
+    @Autowired @Qualifier("PlacesRepository")
+    private PlaceRepository placesRepository;
 
     public FlightService() {
-        this.flightInformationRepository = new FlightInformationRepository();
-        this.placeRepository = new PlaceRepository();
+
     }
 
 
     public List<FlightInformation> getFlightInformation() {
-        return flightInformationRepository.getFlightInformation();
+        return flightsInformationRepository.getFlightInformation();
     }
 
     public List<Place> getPlaces() {
-        return placeRepository.getPlaces();
+        return placesRepository.getPlaces();
 
     }
 
@@ -41,39 +45,35 @@ public class FlightService {
     }
 
 
-    public List<FlightInformation> searchFlights(String source, String destination, int noOfSeats) {
-        return flightInformationRepository.getFlightInformation().stream()
+    public List<FlightInformation> searchFlightsWithSourceAndDestination(String source, String destination) {
+        return flightsInformationRepository.getFlightInformation().stream()
                 .filter(x -> x.getSource().equals(source))
                 .filter(x -> x.getDestination().equals(destination))
                 .collect(Collectors.toList());
     }
 
-    public List<FlightInformation> searchFlightsWithDepartureDate(String source,
-                                                                  String destination, int noOfSeats,
-                                                                  Optional<LocalDate> departureDate,
-                                                                  TravelClass.TravelType travelClass)
+    public List<FlightInformation> searchFlights(FlightSearchCriteria searchCriteria)
     {
-
-        List<FlightInformation> getFlightsWithSearchDate;
-        List<FlightInformation> getFlightsWithoutSearchDate = searchFlights(source,destination,noOfSeats);
-        if(Optional.ofNullable(departureDate).equals(Optional.empty()))
+        List<FlightInformation> getFlights;
+        List<FlightInformation> getFlightsWithSourceAndDestination = searchFlightsWithSourceAndDestination(searchCriteria.getSource(),
+                searchCriteria.getDestination());
+        if(Optional.ofNullable(searchCriteria.getParsedDate()).equals(Optional.empty()))
         {
 
-            getFlightsWithSearchDate = getFlightsWithoutSearchDate.stream()
+            getFlights = getFlightsWithSourceAndDestination.stream()
                     .filter(x -> x.getDepartureDate().isEqual(LocalDate.now()) || x.getDepartureDate().isAfter(LocalDate.now()))
-                    .filter(x -> x.getNumberOfSeatsAvailable(travelClass) > noOfSeats)
+                    .filter(x -> x.getNumberOfSeatsAvailable(searchCriteria.getParsedTravelClass()) >= searchCriteria.getNoOfPassengers())
                     .collect(Collectors.toList());
 
         }
         else
         {
-            getFlightsWithSearchDate = getFlightsWithoutSearchDate.stream()
-                    .filter(x -> x.getDepartureDate().isEqual(departureDate.get()))
-                    .filter(x -> x.getNumberOfSeatsAvailable(travelClass) > noOfSeats)
+            getFlights = getFlightsWithSourceAndDestination.stream()
+                    .filter(x -> x.getDepartureDate().isEqual(searchCriteria.getParsedDate().get()))
+                    .filter(x -> x.getNumberOfSeatsAvailable(searchCriteria.getParsedTravelClass()) >= searchCriteria.getNoOfPassengers())
                     .collect(Collectors.toList());
         }
-        return getFlightsWithSearchDate;
-
+        return getFlights;
     }
 
 
